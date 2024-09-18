3024de67a283723284de18cc50529076bcdcfd7c
sharadaade <aadesharad@gmail.com>
2024-09-18 17:14:10 +0530

    Hashtable(non-generic) nad Dictionary(generic)

---
+++ Durga/GenericDictionaryCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durga
+{
+    internal class GenericDictionaryCollection
+    {
+        static void Main(string[] args)
+        {
+            //Key Differences printing with foreach loop:-
+            //For Dictionary, you use KeyValuePair<TKey, TValue> in the foreach.
+            //For Hashtable, you use DictionaryEntry in the foreach.
+
+
+            // Use foreach to iterate through key-value pairs
+            //foreach (KeyValuePair<string, int> pair in dict)
+            //{
+            //    Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+            //}
+
+
+            Dictionary<int, string> dc = new Dictionary<int, string>();
+            dc.Add(3, "Sharad");
+            dc.Add(1, "Viraj");
+            dc.Add(2, "Vilas");
+            dc.Add(7, "Rajesh");
+            dc.Add(5, "Shubham");
+            dc.Add(9, "Satish");
+
+            //Console.WriteLine(dc.Count);
+            //Console.WriteLine(dc.ContainsKey(1));
+            //Console.WriteLine(dc.ContainsValue("Sharad"));
+            //Console.WriteLine();
+            //dc.Clear();
+
+            foreach (KeyValuePair<int, string> item in dc)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+
+
+        }
+    }
+}
---
+++ Durga/NonGenericHashTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durga
+{
+    internal class NonGenericHashTable
+    {
+        static void Main(string[] args)
+        {
+            // Hashtable:
+            // Hashtable is a collection that stores key-value pairs
+            // Internally for every hashkey unique hashcode are assigned
+            // The keys in a Hashtable are unique, and the values can be accessed using the corresponding key.
+            // The values can be accessed using the corresponding key.
+            // Hashtable stores objects, so boxing and unboxing might occur when storing value types.
+            // For better performance and type safety, Dictionary<TKey, TValue> (generic version) is often preferred.
+
+            //Key Differences foreach loop:-
+            //For Dictionary, you use KeyValuePair<TKey, TValue> in the foreach.
+            //For Hashtable, you use DictionaryEntry in the foreach.
+
+
+            // Use foreach to iterate through key-value pairs
+            //foreach (DictionaryEntry entry in hashtable)
+            //{
+            //    Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
+            //}
+
+
+            Hashtable ht = new Hashtable();
+
+            ht.Add("key1", 123);
+            ht.Add("key2", 456);
+            ht.Add("key3", 789);
+            ht.Add("key4", 987);
+            ht.Add(999, "lll");
+            // Key can not be duplicate or null 
+            //ht.Add("key4", 555);
+
+            // 
+            Console.WriteLine(ht.ContainsKey(999));
+            Console.WriteLine(ht.ContainsValue(654));
+
+            // Clear() => It remove the all element/object from Hashtable
+
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+        }
+    }
+}
